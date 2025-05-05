9905723fdd62c384b1ad6f014b7ef9b6b7e8e303
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-05-04 22:46:05 -0700

    interpreter code: - Create class for model - Add empty key/value list for variable names

--- peeleScript.py
+++ peeleScript.py
@@ -1,3 +1,14 @@
 from textx import metamodel_from_file
 peele_mm = metamodel_from_file('peeleScript.tx')
 peele_model = peele_mm.model_from_file('program.ps')
+
+class peeleScript:
+
+    def __init__(self):
+        self.variables = {}
+        studentNames = ['A-Aron', 'Jayquelin', 'Ba-la-kay', 'Dee-Nice', 'Tim-o-thy']
+        for student in studentNames:
+            self.variables[student] = None
+
+    def interpret(self, model):
+        
