bcf53dbb6c3f745e94903216b29ab347d443d877
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-05-05 18:21:34 -0700

    feat(peeleScript.py): attendance check - Add code to interpret attendance statement

--- peeleScript.py
+++ peeleScript.py
@@ -4,11 +4,21 @@ peele_model = peele_mm.model_from_file('program.ps')
 
 class peeleScript:
 
-    def __init__(self):
+    def __init__(self): 
         self.variables = {}
         studentNames = ['A-Aron', 'Jayquelin', 'Ba-la-kay', 'Dee-Nice', 'Tim-o-thy']
         for student in studentNames:
             self.variables[student] = None
 
     def interpret(self, model):
+        for c in model.statements:
+            if c.__class__.__name__ == "Attendance":
+                if self.variables[c.name] is not None:
+                    print(f"{c.name} is present!")
+                else:
+                    print(f"{c.name} is absent!") 
+            else:
+                print(f"Testing error")
         
+interpreter = peeleScript()
+interpreter.interpret(peele_model)
